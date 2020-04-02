09ba95f87abb1c6af277d211bdf231684d0e671b
salman1256 <salman1256@yahoo.com>
2020-04-02 16:49:16 +0530

    After adding Controller & View

---
+++ Controllers/EmpsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OurWebAppSecond.Models;
+
+namespace OurWebAppSecond.Controllers
+{
+    public class EmpsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Emp> objemplist = new List<Emp>() { 
+            new Emp{ 
+            Id=1,Name="Sam",Desig="Manager"} ,
+             new Emp{
+            Id=2,Name="Jatin",Desig="Manager"},
+             new Emp{
+            Id=4,Name="Mohan",Desig="Manager"},
+             new Emp{
+            Id=7,Name="Anoop",Desig="Manager"}};
+            return View(objemplist);
+        }
+    }
+}
