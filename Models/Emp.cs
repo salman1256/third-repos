236126f87ec1b54e76a3084e02eccec2917b0446
salman1256 <salman1256@yahoo.com>
2020-04-02 16:43:20 +0530

    Seoncd Commit modified:   .vs/OurWebAppSecond/v16/.suo

---
+++ Models/Emp.cs
@@ -0,0 +1,10 @@
+﻿
+namespace OurWebAppSecond.Models
+{
+    public class Emp
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; }
+        public string  Desig { get; set; }
+    }
+}
